50f5b42376d1cb1243fb6b7b06b079883aa0863a
Cedric Louyot <cedric.louyot@gmail.com>
2023-01-29 13:07:25 +0100

    with getDistanceFromLine (ordered)

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ const geolib = require('geolib');
 
 // Path to GPX files to read
 const refPath = './gpx/ref.gpx';
-// const challPath = './gpx/chall-detour.gpx';
-// const challPath = './gpx/chall-start-after.gpx';
-// const challPath = './gpx/chall-start-before.gpx';
-// const challPath = './gpx/chall-autre-chemin.gpx';
 const challPath = './gpx/chall-autre-chemin-2-fois.gpx';
 
 // Params
@@ -39,44 +35,71 @@ const generateGpx = async (segments) => {
 }
 
 const main = async () => {
+  // Load files
   const refPromise = fs.readFile(refPath, { encoding: 'utf8' });
   const challPromise = fs.readFile(challPath, { encoding: 'utf8' });
   const [refStr, challStr] = await Promise.all([refPromise, challPromise]);
 
+  // Create parsers
   refGpx.parse(refStr);
   challGpx.parse(challStr);
 
+  // Create points arrays
   // TODO - Handle GPX with multiple tracks or multiple track segments
   const refPoints = refGpx.tracks[0].points;
   const challPoints = challGpx.tracks[0].points;
 
-  const missedSegments = [];
-  refPoints.forEach((point, index) => {
-    const nearest = geolib.findNearest(point, challPoints);
-    const dist = geolib.getDistance(point, nearest);
+  // Initialize the missedSegments array
+  // This array will contain segment arrays
+  // Each segment array will contain consecutive missed trackpoints
+  const missedSegments = [[]];
 
-    if (dist > tolerance) {
-      // Update point with index
-      point.index = index;
+  let challIndex = 0;
 
-      if (missedSegments.length === 0) {
-        missedSegments.push([]);
-      }
+  // Loop through all reference track points
+  refIndexLoop:
+  for (let refIndex = 0; refIndex < refPoints.length; refIndex++) {
+    const refPoint = refPoints[refIndex];
 
-      let lastMissedSegment = missedSegments[missedSegments.length - 1];
-
-      if (lastMissedSegment.length === 0 ||
-          lastMissedSegment[lastMissedSegment.length - 1].index === index - 1) {
-        lastMissedSegment.push(point);
-      } else {
-        missedSegments.push([point]);
+    for (let challLocalIndex = challIndex; challLocalIndex < challPoints.length - 1; challLocalIndex++) {
+      const dist = geolib.getDistanceFromLine(
+        refPoint,
+        challPoints[challLocalIndex],
+        challPoints[challLocalIndex + 1]
+      );
+      if (dist <= tolerance) {
+        challIndex = challLocalIndex;
+        continue refIndexLoop;
       }
     }
-  });
 
+    // Passing here only when challenger track
+    // didn't pass close enough (i.e. d < tolerance)
+    // from the reference track.
+    // In this case, add the missed refPoint to the missedSegments array
+    refPoint.index = refIndex;
+
+    let lastMissedSegment = missedSegments[missedSegments.length - 1];
+
+    // Append to lastMissedSegment only if current missing trackpoint from reference
+    // immediatly follows the last one added.
+    // If not create a new segment in missedSegments.
+    if (lastMissedSegment.length === 0 ||
+        lastMissedSegment[lastMissedSegment.length - 1].index === refIndex - 1) {
+      lastMissedSegment.push(refPoint);
+    } else {
+      missedSegments.push([refPoint]);
+    }
+  };
+
+  // Generate the file containing the missed segments
   missedGpx = await generateGpx(missedSegments)
+
+  // Calculate and display synthesis
   const missedDistance = missedGpx.tracks.reduce((acc, track) => acc + track.distance.total, 0);
-  console.log(missedDistance);
+  const missedPercent = missedDistance / refGpx.tracks[0].distance.total * 100
+  console.log(`Missed ${Math.round(missedDistance)} meters of the reference path`)
+  console.log(`Missed ${Math.round(missedPercent * 10) / 10} % of the reference path`)
 }
 
 main();
