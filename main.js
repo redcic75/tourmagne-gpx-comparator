86b5af0aeccfd49d996de366f43b26c69d7637a8
Cedric Louyot <cedric.louyot@gmail.com>
2023-01-29 11:42:15 +0100

    generates a gpx file containing missed track points

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const geolib = require('geolib');
 // Path to GPX files to read
 const refPath = './gpx/ref.gpx';
 // const challPath = './gpx/chall-detour.gpx';
+// const challPath = './gpx/chall-start-after.gpx';
+// const challPath = './gpx/chall-start-before.gpx';
 // const challPath = './gpx/chall-autre-chemin.gpx';
 const challPath = './gpx/chall-autre-chemin-2-fois.gpx';
 
@@ -13,38 +15,68 @@ const tolerance = 20; // in meters
 
 // Parsers for reference route and challenger track
 const refGpx = new gpxParser();
-const challGpx = new gpxParser();
+const challGpx = new gpxParser();;
 
-async function main() {
+const generateGpx = async (segments) => {
+  // Generate GPX string from segments
+  let gpxStr = '<xml><gpx>';
+  segments.forEach((seg) => {
+    gpxStr += '<trk><trkseg>'
+    seg.forEach((point) => {
+      gpxStr += `<trkpt lat="${point.lat}" lon="${point.lon}"></trkpt>`
+    });
+    gpxStr += '</trkseg></trk>'
+  });
+  gpxStr += '</gpx></xml>';
+
+  // Write a GPX file
+  await fs.writeFile('./generated_files/missed.gpx', gpxStr);
+
+  // Generates a missedGpx object and returns it
+  const missedGpx = new gpxParser();
+  missedGpx.parse(gpxStr);
+  return missedGpx;
+}
+
+const main = async () => {
   const refPromise = fs.readFile(refPath, { encoding: 'utf8' });
   const challPromise = fs.readFile(challPath, { encoding: 'utf8' });
-  const [refFile, challFile] = await Promise.all([refPromise, challPromise]);
+  const [refStr, challStr] = await Promise.all([refPromise, challPromise]);
 
-  refGpx.parse(refFile);
-  challGpx.parse(challFile);
+  refGpx.parse(refStr);
+  challGpx.parse(challStr);
 
   // TODO - Handle GPX with multiple tracks or multiple track segments
   const refPoints = refGpx.tracks[0].points;
   const challPoints = challGpx.tracks[0].points;
 
-  const refPointsMissed = [];
+  const missedSegments = [];
   refPoints.forEach((point, index) => {
     const nearest = geolib.findNearest(point, challPoints);
     const dist = geolib.getDistance(point, nearest);
+
     if (dist > tolerance) {
+      // Update point with index
       point.index = index;
-      refPointsMissed.push(point);
+
+      if (missedSegments.length === 0) {
+        missedSegments.push([]);
+      }
+
+      let lastMissedSegment = missedSegments[missedSegments.length - 1];
+
+      if (lastMissedSegment.length === 0 ||
+          lastMissedSegment[lastMissedSegment.length - 1].index === index - 1) {
+        lastMissedSegment.push(point);
+      } else {
+        missedSegments.push([point]);
+      }
     }
   });
 
-  console.log(refPointsMissed);
+  missedGpx = await generateGpx(missedSegments)
+  const missedDistance = missedGpx.tracks.reduce((acc, track) => acc + track.distance.total, 0);
+  console.log(missedDistance);
 }
 
 main();
-
-/**
- * Syntax
-const totalDistance = gpx.tracks[0].distance.total;
-gpx.tracks[0].points[2];
-const dist = geolib.getDistance(gpx.tracks[0].points[2], gpx.tracks[0].points[5]);
- */
